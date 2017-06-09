6bb59634f95f8e9fbb41f1f3002dd8b2dd7d484f
kirihar2 <kirihar2@illinois.edu>
2017-06-08 19:18:05 -0500

    Partial title screen finished. Bug where touch ends the color changes and is unable to touch after it

--- rainfall/rainfall/GameViewController.swift
+++ rainfall/rainfall/GameViewController.swift
@@ -17,17 +17,18 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
             if let titleScene = SKScene(fileNamed: "TitleScene") {
                 titleScene.scaleMode = .aspectFill
                 view.presentScene(titleScene)
             }
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//                
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+            
             
             view.ignoresSiblingOrder = true
             
--- rainfall/rainfall/TitleScene.swift
+++ rainfall/rainfall/TitleScene.swift
@@ -10,18 +10,49 @@ import SpriteKit
 import GameplayKit
 
 class TitleScene: SKScene {
-    let backgroundRain = SKEmitterNode(fileNamed: "TitleRainBackground")
-    let frame_width = titleScene.frame.size.width
-    
+    var backgroundRain : SKEmitterNode!
+    var frame_width : CGFloat!
+    var frame_height : CGFloat!
+    let density_factor = 0.5
+    private var gameTitle: SKLabelNode!
+    private var startGameButton: SKLabelNode!
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
+
+        frame_width = scene!.frame.size.width
+        frame_height = scene!.frame.size.height
+        let center = CGPoint(x: frame_width/2, y: frame_height/2)
+
+        backgroundRain = SKEmitterNode(fileNamed: "TitleRainBackground")
+        backgroundRain.position =  CGPoint(x: frame_width/2, y: frame_height)
+        backgroundRain.particlePositionRange = CGVector(dx: frame_width * 1.5, dy: frame_height * 0.05)
+        backgroundRain.particleBirthRate = frame_width * CGFloat(density_factor)
+        addChild(backgroundRain)
         
-        backgroundRain?.position = CGPoint() 
+        gameTitle = SKLabelNode(fontNamed: "Chalkduster")
+        gameTitle.fontSize = 50
+        gameTitle.text = "Rainfall"
+        gameTitle.name = "gameTitle"
+        gameTitle.position = CGPoint(x: frame_width/2, y: frame_height - gameTitle.fontSize)
+        gameTitle.zPosition = 1
+        addChild(gameTitle)
+        
+        startGameButton = SKLabelNode(fontNamed: "Chalkduster")
+        startGameButton.text = "Start Game"
+        startGameButton.position = center
+        startGameButton.fontSize = 40
+        startGameButton.zPosition = 1
+        startGameButton.name = "startGameButton"
+        addChild(startGameButton)
         
         
     }
-    
+    private func startGame() {
+        let gameScene = GameScene(size: scene!.frame.size)
+        let transition = SKTransition.fade(withDuration: 0.5)
+        view!.presentScene(gameScene, transition: transition)
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         
@@ -36,7 +67,15 @@ class TitleScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
+        guard let touch = touches.first else {return }
+        
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        for node in nodesAtPoint {
+            if node.name == "startGameButton"{
+                startGame()
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
